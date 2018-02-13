c1dd0f1173f2f39473c8a580d09f393deb489621
darkpandaprog <darkpandaprog@gmail.com>
2018-02-14 00:03:18 +0200

    Project is ready to deploy.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { trigger } from '@angular/animations';
+import { transition } from '@angular/animations';
+import { animate, style } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [ './app.component.css' ],
+  animations: [
+    trigger('fade', [
+      transition('void => *', [
+        style({opacity: 0}),
+        animate(4000, style({opacity: 1}))
+      ])
+    ]),
+    trigger('moving-header', [
+      transition('void => *', [
+        style({height: 0}),
+        animate(500)
+      ])
+    ])
+  ]
 })
 export class AppComponent {
   title = 'app';
---
+++ src/assets/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $("#modal_close").click(function() {
+        $("#modal").hide();
+    });
+    $(".closer_modal").click(function() {
+        $("#modal").hide();
+    })
+});
